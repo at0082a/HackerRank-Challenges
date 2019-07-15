f5b30f0a44a1d73185715cb4229bc9858937bcbe
at0082a <alex.tilatti@gmail.com>
2019-07-15 00:14:14 -0400

    Update Breaking-Records.js

--- Breaking-Records.js
+++ Breaking-Records.js
@@ -10,15 +10,19 @@ function breakingRecords(scores) {
     let newarr = []
     let recordcount = scores[0] 
     let losscount = scores[0]
+    
     for (let i = 1; i < scores.length; i++) {
+//  if statement for breaking highest score record
         if (scores[i] > scores[i - 1] && scores[i] > recordcount) {
             arr.push(i)
             recordcount = scores[i]
         }
+//  if statement for breaking lowest score record
         if (scores[i] < scores[i - 1] && scores[i] < losscount) {
             newarr.push(i)
             losscount = scores[i]
         }
     }
+    // return the amount of times the highest scoring record and lowest scoring record are broken
     return [arr.length, newarr.length]
 }
