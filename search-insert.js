93ad33fc8556fed6699dbfbedb90267bd1045ec2
Alex Tilatti <alex.tilatti@gmail.com>
2019-10-20 21:59:30 -0400

    Update search-insert.js

--- search-insert.js
+++ search-insert.js
@@ -1,17 +1,20 @@
+// Given a sorted array and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+
 function searchInsert (nums, target) {
    let count = 0
-    if (nums.indexOf(target) === -1) {
-        for (let i = 0; i < nums.length; i++) {
-          if (nums[i] < target) {
-            count += 1
-          }
-      }
-        return count
-    } else {
+   if (nums.indexOf(target) === -1) {
+   for (let i = 0; i < nums.length; i++) {
+       if (nums[i] < target) {
+         count += 1
+       }
+   }
+     return count
+   } else {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === target) {
           return i
         }
-    }
+     }
   } 
 }
